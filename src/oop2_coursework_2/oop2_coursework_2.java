91f0c4aa8e9720b75bcf1836951e59bd93fa486b
JanZ <janz@server.example.com>
2019-02-12 20:42:01 +0300

    deleted everything

--- src/oop2_coursework_2/oop2_coursework_2.java
+++ src/oop2_coursework_2/oop2_coursework_2.java
@@ -1,9 +1,12 @@
 package oop2_coursework_2;
+import java.util.*;
 
 public class oop2_coursework_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
 
 	}
 
