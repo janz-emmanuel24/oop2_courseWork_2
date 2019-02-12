706f1a8cbead6b23b587650977029c722afeb3b4
JanZ <janz@server.example.com>
2019-02-12 20:54:15 +0300

    added variables and scanner class

--- src/oop2_coursework_2/oop2_coursework_2.java
+++ src/oop2_coursework_2/oop2_coursework_2.java
@@ -4,10 +4,21 @@ import java.util.*;
 public class oop2_coursework_2 {
 
 	public static void main(String[] args) {
+		//Creating Arraylists to hold the marks, names and courses
 		ArrayList<String> stuNames = new ArrayList<String>();
 		ArrayList<String> stuCourses = new ArrayList<String>();
 		ArrayList<Integer> stuMarks = new ArrayList<Integer>();
+		//Creating variables
+		String stuName, stuCourse;
+		int stuMark;
 		
+		Scanner _insert = new Scanner(System.in);
+		System.out.println("Enter Student Name: ");
+		stuName = _insert.nextLine();
+		System.out.println("Enter Student course: ");
+		stuCourse = _insert.nextLine();
+		System.out.println("Enter Studen Course mark: ");
+		stuMark = _insert.nextInt();
 		
 
 	}
