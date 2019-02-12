ca467c8be4364c01f4bd795e62f3be9224a1acc3
JanZ <janz@server.example.com>
2019-02-12 20:44:55 +0300

    added 3 arrayLists

--- src/oop2_coursework_2/oop2_coursework_2.java
+++ src/oop2_coursework_2/oop2_coursework_2.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class oop2_coursework_2 {
 
 	public static void main(String[] args) {
-		
+		ArrayList<String> stuNames = new ArrayList<String>();
+		ArrayList<String> stuCourses = new ArrayList<String>();
+		ArrayList<Integer> stuMarks = new ArrayList<Integer>();
 		
 		
 
