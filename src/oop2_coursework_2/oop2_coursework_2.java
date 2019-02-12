1422916a7b7b5521f1f9d577fdb140df744058e8
JanZ <janz@server.example.com>
2019-02-12 21:31:22 +0300

    students, courses and marks can be entered

--- src/oop2_coursework_2/oop2_coursework_2.java
+++ src/oop2_coursework_2/oop2_coursework_2.java
@@ -14,7 +14,7 @@ public class oop2_coursework_2 {
 		
 		Scanner _insert = new Scanner(System.in);// Scanner variable
 		
-		for (int i = 0; i <= stuNames.size(); i++) { //Start of the first loop
+		for (int i = 0; i <= stuNames.size();) { //Start of the first loop
 			if (stuNames.size() == 6) {
 				break;
 			} else 
@@ -22,14 +22,25 @@ public class oop2_coursework_2 {
 				stuName = _insert.nextLine();
 				stuNames.add(stuName); //End of first Loop
 				
+				for (int k = 0; k <= stuCourses.size(); k++) { //Second Loop
+					if (stuCourses.size() == stuNames.size()) {
+						break;
+					} else 
+						System.out.println("Enter Student course: ");
+						stuCourse = _insert.nextLine();
+						stuCourses.add(stuCourse);
+						for (int j = 0; j <= stuMarks.size(); j++) { //third Loop
+							if (stuMarks.size() == stuCourses.size()) {
+								break;
+							} else
+								System.out.println("Enter Student Course mark: ");
+								stuMark = _insert.nextInt();
+								stuMarks.add(stuMark);
+						} //third loop ends
+				} //end of second loop
+				
+				i++;
 		}
-		
-		System.out.println("Enter Student course: ");
-		stuCourse = _insert.nextLine();
-		System.out.println("Enter Studen Course mark: ");
-		stuMark = _insert.nextInt();
-		
-
 	}
 
 }
